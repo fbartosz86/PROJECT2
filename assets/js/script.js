ecf3f4b3253c5457195b2d553b9c798a515929f4
fbartosz86 <>
2023-08-04 21:57:39 +0000

    added data-seclections atrybiuts and test the event listener of the draft buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
-// declaring variables 
+
 const draftButtons = document.querySelectorAll('[data-selection]');
+
+
+
+draftButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection;
+        makeSelection(selectionName);
+    });
+});
+
+function makeSelection(selection) {
+    console.log(selection);
+}
